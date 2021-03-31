f05b4153aaa155c04072d7d2478dfdf271840e0b
jaymody <jaykmody@gmail.com>
2021-03-31 11:38:31 -0400

    added animations

--- main.py
+++ main.py
@@ -13,13 +13,32 @@ blue = (0, 0, 255)
 white = (255, 255, 255)
 black = (0, 0, 0)
 
+# click for minigame
+# add game update text
+# add animations
+
+
+def cubic_distance(x, b, c):
+    # accelerate / decelerate motion
+    # linear accelaration, quadratic speed, cubic distance
+    # speed = -ax^2 + abx
+    # x = time
+    # b = time needed to reach final position (max time)
+    # c = final position
+    assert b >= 0
+    assert c >= 0
+    assert x >= 0
+    assert x <= b
+    a = 6 * c / b ** 3
+    return -a / 3 * x ** 3 + a * b / 2 * x ** 2
+
 
 class Game:
-    def __init__(self, screen, font, w, h, num_players):
-        """Initialize a new game."""
+    def __init__(self, screen, clock, font, w, h, num_players):
         self.board = Board()
 
         self.screen = screen
+        self.clock = clock
         self.font = font
 
         s = h // 12
@@ -27,6 +46,8 @@ class Game:
         self.squares = [Rect(s + c * s, h - 2 * s - r * s, s, s) for r, c in coords]
 
         self.button_roll = Rect(s * 12, s * 1, s * 4, s)
+        self.prompt_location = Rect(s * 12, s * 3, s * 4, s * 4).center
+        self.prompt = ""
 
         self.w, self.h, self.s = w, h, s
 
@@ -42,38 +63,59 @@ class Game:
         # TODO: implement this
         return random.random() > 0.5
 
-    def animate_move(self):
-        pass
+    def animate_cubic(self, v1, v2, draw_fn, seconds):
+        v3 = v2 - v1
+        ticks = int(seconds * 30)
 
-    def animate_snake(self):
-        pass
+        for t in range(ticks):
+            draw_fn((v1 + v3 * cubic_distance(t, ticks, ticks) / ticks).xy)
+            pygame.display.update()
+            self.clock.tick(30)
 
-    def animate_ladder(self):
-        pass
+    def animate_player(self, p, v1, v2, seconds=0.3):
+        def draw_fn(xy):
+            self.draw_board()
+            self.draw_players(ignore_player=p)
+            self.screen.blit(
+                self.font.render(str(p + 1), True, black),
+                xy,
+            )
+
+        self.animate_cubic(v1, v2, draw_fn, seconds)
+
+    def animate_move(self, p, n):
+        if n > 0:
+            positions = [self.players[p] + i for i in range(min(n + 1, 99))]
+        else:
+            positions = [self.players[p] - i for i in range(min(abs(n + 1), 99))]
+
+        for old, new in zip(positions[:-1], positions[1:]):
+            v1 = pygame.math.Vector2(self.squares[old].center)
+            v2 = pygame.math.Vector2(self.squares[new].center)
+            self.animate_player(p, v1, v2)
+
+    def animate_snake(self, p, head, tail):
+        v1 = pygame.math.Vector2(self.squares[head].center)
+        v2 = pygame.math.Vector2(self.squares[tail].center)
+        self.animate_player(p, v1, v2, seconds=1)
+
+    def animate_ladder(self, p, bottom, top):
+        v1 = pygame.math.Vector2(self.squares[bottom].center)
+        v2 = pygame.math.Vector2(self.squares[top].center)
+        self.animate_player(p, v1, v2, seconds=1)
 
     def animate_roll(self):
         pass
 
     def play_turn(self, p, backwards=False):
-        """Runs a turn for the
-
-        Parameters
-        ----------
-        p : int
-            Player number.
-
-        Returns
-        -------
-        bool
-            True if the p has won.
-        """
-
         # increments current player pos by dice roll
         # TODO: let player hit the roll button
+        n = self.roll()
         if backwards:
-            self.players[p] -= self.roll()
-        else:
-            self.players[p] += self.roll()
+            n = -n
+        self.prompt = f"Player {(self.turn % len(self.players)) + 1} rolled a {n}"
+        self.animate_move(p, n)
+        self.players[p] += n
 
         if self.players[p] >= 99:
             # they've won the game
@@ -90,10 +132,16 @@ class Game:
         if square == sq_snake:
             minigame_won = self.play_minigame()
             if not minigame_won:
+                self.animate_snake(
+                    p, self.players[p], self.board.snakes[self.players[p]]
+                )
                 self.players[p] = self.board.snakes[self.players[p]]
         elif square == sq_ladder:
             minigame_won = self.play_minigame()
             if minigame_won:
+                self.animate_ladder(
+                    p, self.players[p], self.board.ladders[self.players[p]]
+                )
                 self.players[p] = self.board.ladders[self.players[p]]
         elif square == sq_special:
             minigame_won = self.play_minigame()
@@ -104,8 +152,15 @@ class Game:
         else:
             return False
 
-    def display(self):
+    def draw_board(self):
         self.screen.fill(white)
+
+        pygame.draw.rect(self.screen, blue, self.button_roll)
+        self.screen.blit(self.font.render("Roll", True, white), self.button_roll.center)
+
+        self.screen.blit(
+            self.font.render(self.prompt, True, black), self.prompt_location
+        )
 
         for i, sq in enumerate(self.squares):
             pygame.draw.rect(self.screen, black, sq, width=3)
@@ -137,19 +192,20 @@ class Game:
                 radius=4,
             )
 
-        pygame.draw.rect(self.screen, blue, self.button_roll)
-
-        for i, player in enumerate(self.players):
-            self.screen.blit(
-                self.font.render(str(i + 1), True, black),
-                self.squares[player].center,
-            )
-
-        pygame.display.update()
+    def draw_players(self, ignore_player=None):
+        for p, player in enumerate(self.players):
+            if p != ignore_player:
+                self.screen.blit(
+                    self.font.render(str(p + 1), True, black),
+                    self.squares[player].center,
+                )
 
     def play_game(self):
         """Main game loop."""
-        self.display()
+        self.prompt = f"Player {(self.turn % len(self.players)) + 1}'s turn"
+        self.draw_board()
+        self.draw_players()
+        pygame.display.update()
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -160,11 +216,16 @@ class Game:
                     mouse_pos = event.pos
                     if self.button_roll.collidepoint(mouse_pos):
                         if self.play_turn(self.turn % len(self.players)):
-                            print(f"p {(self.turn %  len(self.players))+1} WON")
+                            print(f"p {(self.turn % len(self.players))+1} WON")
                             return
                         self.turn += 1
 
-                self.display()
+                        self.prompt += (
+                            f", Player {(self.turn % len(self.players)) + 1}'s turn"
+                        )
+                        self.draw_board()
+                        self.draw_players()
+                        pygame.display.update()
 
 
 class Board:
@@ -173,7 +234,6 @@ class Board:
     SPECIAL_SQUARE = 2
 
     def __init__(self):
-        """Board constructor."""
         # {head:tail, bottomladder:topladder, etc}
         # snake heads call minigame
         self.snakes = {96: 77, 94: 55, 87: 23, 61: 17, 47: 25, 35: 5, 31: 9}
@@ -181,32 +241,22 @@ class Board:
         self.ladders = {1: 37, 3: 13, 7: 29, 27: 73, 20: 41, 49: 66, 70: 91, 79: 98}
 
         # generate 5 random positions for the special squares
-        # don't include square 0 or square 99\
-        used_squares = set(self.snakes.keys()) | set(self.ladders.keys())
+        # don't include square 0 or square 99
+        used_squares = (
+            set(self.snakes.keys())
+            | set(self.ladders.keys())
+            | set(self.snakes.values())
+            | set(self.snakes.keys())
+        )
         unused_squares = set(range(1, 99)) - used_squares
         self.special = random.sample(unused_squares, 5)
 
     def get_coord(self, pos):
-        """Get coordinate on board (row, column) from bottom to top.
-
-        Parameters
-        ----------
-        pos : int
-            Position from 0 to 99.
-
-        Returns
-        -------
-        row, col : int, int
-            Integer pair containing row and column of the position. Row 0 is
-            bottom most row, column 0 is leftmost row.
-        """
         row = pos // 10
         col = pos % 10 if row % 2 == 0 else (9 - pos % 10)
         return row, col
 
     def get_square(self, pos):
-        """Gets if a position is on a snake/ladder/special/normal square."""
-
         if pos in self.snakes:
             return self.SNAKE_SQUARE
         elif pos in self.ladders:
@@ -224,7 +274,7 @@ if __name__ == "__main__":
 
     pygame.init()
     pygame.font.init()
-    font = pygame.font.SysFont("timesnewroman", 14)
+    font = pygame.font.SysFont("timesnewroman", 20)
     screen = pygame.display.set_mode([width, height])
     pygame.display.set_caption("Super Snakes and Ladders")
     clock = pygame.time.Clock()
@@ -243,7 +293,7 @@ if __name__ == "__main__":
     # main menu
     menu = pygame_menu.Menu("Super Snakes and Ladders", width, height, theme=theme)
     menu.add.button(
-        "Play", lambda: Game(screen, font, w=width, h=height, num_players=2)
+        "Play", lambda: Game(screen, clock, font, w=width, h=height, num_players=2)
     )
     menu.add.button("Help", help_menu)
     menu.add.button("Settings", settings_menu)
